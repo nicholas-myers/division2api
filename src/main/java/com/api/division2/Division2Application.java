b33a0cc6cbf7f73f8b8af8965acbd55415f5dadf
Nicholas Myers <nicholas.myers.porfessional@gmail.com>
2020-09-30 18:54:19 -0700

    created intellij project with springframework dependencies

---
+++ src/main/java/com/api/division2/Division2Application.java
@@ -0,0 +1,15 @@
+package com.api.division2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Division2Application
+{
+
+   public static void main(String[] args)
+   {
+      SpringApplication.run(Division2Application.class, args);
+   }
+
+}
